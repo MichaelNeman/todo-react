ed5ac146bbb488524a9358c7acdfdcaa021e817a
michaelFeMan <mneman1702@gmail.com>
2021-01-21 20:35:57 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>To Do App</h1>
+        <form>
+          <h2>To do List</h2>
+          <input type="text" id="new-todo-input" className="input input_large" name="text" autoComplete="off"/>
+          <button type="submit" className="btn btn_primary btn_lg">Add</button>
+        </form>
+        <div className="filters btn-group stack-exception">
+          <button type="button" className="btn toggle-btn" aria-pressed="true">Show all tasks</button>
+          <button type="button" className="btn toggle-btn" aria-pressed="false">Show active tasks</button>
+          <button type="button" className="btn toggle-btn" aria-pressed="false">Show completed tasks</button>
+        </div>
+        <h2 id="list-heading">3 tasks remaining</h2>
+        <ul className="todo-list stack-large stack-exception" aria-labelledby="list-heading">
+          <li className="todo stack-small">
+            <div classname="c-cb">
+              <input id="todo-0" type="checkbox" defaultChecked={false} />
+              <label className="todo-label" htmlFor="todo-0">
+                Eat
+              </label>
+            </div>
+            <div className="btn-group">
+              <button type="button" className="btn">
+                Edit <span className="visually-hidden">Eat</span> </button>
+                <button type="button" className="btn btn__danger">Delete <span className="visually-hidden">Eat</span></button>
+            </div>
+          </li>
+          <li className="todo stack-small">
+            <div classname="c-cb">
+              <input id="todo-1" type="checkbox" defaultChecked={false} />
+              <label className="todo-label" htmlFor="todo-1">
+                Run
+              </label>
+            </div>
+            <div className="btn-group">
+              <button type="button" className="btn">
+                Edit <span className="visually-hidden">Run</span> </button>
+                <button type="button" className="btn btn__danger">Delete <span className="visually-hidden">Run</span></button>
+            </div>
+          </li>
+          <li className="todo stack-small">
+            <div classname="c-cb">
+              <input id="todo-2" type="checkbox" defaultChecked={false} />
+              <label className="todo-label" htmlFor="todo-2">
+                Code
+              </label>
+            </div>
+            <div className="btn-group">
+              <button type="button" className="btn">
+                Edit <span className="visually-hidden">Code</span> </button>
+                <button type="button" className="btn btn__danger">Delete <span className="visually-hidden">Code</span></button>
+            </div>
+          </li>
+        </ul>
+    
     </div>
   );
 }
