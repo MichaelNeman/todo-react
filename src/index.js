4e2907ccff524e4493978fb185d9106bde9dfa25
michaelFeMan <mneman1702@gmail.com>
2021-02-09 20:01:59 -0600

    added functionality to checkboxes and delete button

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 
 const DATA = [
-  {id: 'todo-0', name: 'Eat', completed: true},
+  {id: 'todo-0', name: 'Eat', completed: false},
   {id: 'todo-1', name: 'Code', completed: false},
   {id: 'todo-2', name: 'Run', completed: false}
 ];
