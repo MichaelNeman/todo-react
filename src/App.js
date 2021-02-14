a844262d324ee9601329898b8c5d49f52e2759c4
michaelFeMan <mneman1702@gmail.com>
2021-02-14 14:20:53 -0600

    added functionality to filter buttons. Does not display name once completed, default view on loading hsould be all

--- src/App.js
+++ src/App.js
@@ -3,24 +3,34 @@ import './App.css';
 import Todo from './components/Todo';
 import Form from './components/Form';
 import FilterButton from './components/FilterButton';
-import { nanoid  } from "nanoid";
+import { nanoid } from "nanoid";
 
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: task => !task.completed,
+  Completed: task => task.completed
+};
 
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
+//Begin filter App
 
 function App(props) {
 
   const [tasks, setTasks] = useState(props.tasks);
 
+  const [filter, setFilter] = useState('All');
+
   function addTask(name) {
-    const newTask = {id: 'todo-' + nanoid(), name: name, completed: false };
+    const newTask = { id: 'todo-' + nanoid(), name: name, completed: false };
     setTasks([...tasks, newTask]);
   }
 
   function toggleTaskCompleted(id) {
     const updatedTasks = tasks.map(task => {
       if (id === task.id) {
-        return {...tasks, completed: !task.completed}
+        return { ...tasks, completed: !task.completed }
       }
       return task;
     })
@@ -31,16 +41,36 @@ function App(props) {
     const remainingTasks = tasks.filter(task => id !== task.id);
     setTasks(remainingTasks);
   }
-  
-  const taskList = tasks.map(task => ( 
-  <Todo 
-    id={task.id} 
-    name={task.name} 
-    completed={task.completed} 
-    key={task.id}
-    toggleTaskCompleted={toggleTaskCompleted}
-    deleteTask={deleteTask}
-  />
+
+  function editTask(id, newName) {
+    const editedTaskLIst = tasks.map(task => {
+      if (id === task.id) {
+        return { ...task, name: newName }
+      }
+      return task;
+    });
+    setTasks(editedTaskLIst);
+  }
+
+  const taskList = tasks.filter(FILTER_MAP[filter]).map(task => ( 
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+      editTask={editTask}
+    />
+  ));
+
+  const filterList = FILTER_NAMES.map(name => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
   ));
 
   const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
@@ -48,18 +78,16 @@ function App(props) {
 
   return (
     <div className="App stack-large">
-        <h1>To Do App</h1>
-        <Form addTask={addTask} />
-        <div className="filters btn-group stack-exception">
-          <FilterButton />
-          <FilterButton />
-          <FilterButton />
-        </div>
-        <h2 id="list-heading">{headingText}</h2>
-        <ul className="todo-list stack-large stack-exception" aria-labelledby="list-heading">
-          {taskList}
-        </ul>
-    
+      <h1>To Do App</h1>
+      <Form addTask={addTask} />
+      <div className="filters btn-group stack-exception">
+        {filterList}
+      </div>
+      <h2 id="list-heading">{headingText}</h2>
+      <ul className="todo-list stack-large stack-exception" aria-labelledby="list-heading">
+        {taskList}
+      </ul>
+
     </div>
   );
 }
--- src/components/FilterButton.js
+++ src/components/FilterButton.js
@@ -1,8 +1,15 @@
 import React from "react";
 
 function FilterButton(props) {
+
     return (
-        <button type="button" className="btn toggle-btn" aria-pressed="true">Show all tasks</button>
+        <button type="button"
+            className="btn toggle-btn"
+            aria-pressed={props.isPressed}
+            onClick={() => props.setFilter(props.name)}
+        >
+             {props.name}  
+        </button >
     );
 }
 
